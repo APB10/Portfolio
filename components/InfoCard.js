c7aac6b78e7d1376e9df81b802bb34a5f2841ab2
Ali P. Bouraki <ali_panahi10@hotmail.com>
2021-12-07 15:59:03 +0000

    left content added, blur content background

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -6,14 +6,48 @@ function InfoCard() {
     <div>
       <Header />
       {/* Main container */}
-      <div className="flex   ">
+      <div className="flex text-white m-2 md:m-32 md:space-x-20 space-y-2 sm:space-y-0 flex-col sm:flex-row">
         {/* Left - profile container  */}
-        <div>
-          <p>Test1</p>
+        <div className=" text-center sm:w-1/3  space-y-6 border border-white rounded-lg py-2 sm:py-4 backdrop-blur-sm">
+          <div>
+            <h1 className="md:text-2xl">Ali P. Bouraki</h1>
+            <p>Front-End Developer</p>
+          </div>
+          <h1
+            className="md:text-lg underline font-bold hover:text-[#FD7F20] cursor-pointer "
+            onClick={() =>
+              window.open(
+                "https://drive.google.com/file/d/1mTdSl64JxpMLYPG19GRNmHtn_Lhpfr7E/view?usp=sharing"
+              )
+            }
+          >
+            Resumé
+          </h1>
+          <div>
+            <h2 className=" font-bold pt-2 text-[#FD7F20]">Location</h2>
+            <p>Glasgow, Scotland</p>
+          </div>
+          <div>
+            <h2 className=" font-bold pt-2 text-[#FD7F20]">Mobile</h2>
+            <p>07449 188448</p>
+          </div>
+          <div>
+            <h2 className=" font-bold pt-2 text-[#FD7F20]">E-mail</h2>
+            <p>ali_panahi10@hotmail.com</p>
+          </div>
+          <div>
+            <h2 className=" font-bold pt-2 text-[#FD7F20]">GitHub</h2>
+            <p
+              className=" cursor-pointer hover:text-[#FD7F20] hover:underline"
+              onClick={() => window.open("https://github.com/APB10")}
+            >
+              github.com/APB10
+            </p>
+          </div>
         </div>
 
         {/* Right - content container  */}
-        <div>
+        <div className=" w-full md:w-2/3 border border-white backdrop-blur-sm">
           <p>Test2</p>
         </div>
       </div>
