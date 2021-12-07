a18b4628fc028a14431202e49d89cd6e5e7c56f9
Ali P. Bouraki <ali_panahi10@hotmail.com>
2021-12-07 17:11:59 +0000

    finished left panel on Profile page

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -8,13 +8,14 @@ function InfoCard() {
       {/* Main container */}
       <div className="flex text-white m-2 md:m-32 md:space-x-20 space-y-2 sm:space-y-0 flex-col sm:flex-row">
         {/* Left - profile container  */}
-        <div className=" text-center sm:w-1/3  space-y-6 border border-white rounded-lg py-2 sm:py-4 backdrop-blur-sm">
+        <div className=" text-center sm:w-1/3  space-y-6 border border-[#FD7F20] rounded-lg py-2 sm:py-4 backdrop-blur-sm">
           <div>
             <h1 className="md:text-2xl">Ali P. Bouraki</h1>
             <p>Front-End Developer</p>
           </div>
-          <h1
-            className="md:text-lg underline font-bold hover:text-[#FD7F20] cursor-pointer "
+          <button
+            className="items-center border border-[#FD7F20] md:p-2 rounded-xl  hover:text-[#FD7F20] cursor-pointer 
+            transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
             onClick={() =>
               window.open(
                 "https://drive.google.com/file/d/1mTdSl64JxpMLYPG19GRNmHtn_Lhpfr7E/view?usp=sharing"
@@ -22,14 +23,10 @@ function InfoCard() {
             }
           >
             Resumé
-          </h1>
+          </button>
           <div>
             <h2 className=" font-bold pt-2 text-[#FD7F20]">Location</h2>
             <p>Glasgow, Scotland</p>
-          </div>
-          <div>
-            <h2 className=" font-bold pt-2 text-[#FD7F20]">Mobile</h2>
-            <p>07449 188448</p>
           </div>
           <div>
             <h2 className=" font-bold pt-2 text-[#FD7F20]">E-mail</h2>
@@ -47,7 +44,7 @@ function InfoCard() {
         </div>
 
         {/* Right - content container  */}
-        <div className=" w-full md:w-2/3 border border-white backdrop-blur-sm">
+        <div className=" w-full md:w-2/3 border border-[#FD7F20] backdrop-blur-sm rounded-lg py-2 sm:py-4">
           <p>Test2</p>
         </div>
       </div>
