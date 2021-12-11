c67ee9b9810e597df5c4a23bc4ca24f87a19f016
Ali P. Bouraki <ali_panahi10@hotmail.com>
2021-12-11 18:06:28 +0000

    changed tabs layout and finished its function

--- components/Tabs.js
+++ components/Tabs.js
@@ -1,24 +1,74 @@
 import React from "react";
 import styles from "./Tabs.module.css";
+import { useState } from "react";
 
 function Tabs() {
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
   return (
     <div>
-      <ul className={styles.tabs}>
-        <li className={styles.current}>Employment</li>
-        <li>Academic</li>
-        <li>Skills</li>
-      </ul>
-      <div className="mx-2 mt-4">
-        <p>
-          s its finest peacekeeper, Caitlyn is also Piltover’s best shot at
-          ridding the city of its elusive criminal elements. She is often paired
-          with Vi, acting as a cool counterpoint to her partner’s more impetuous
-          nature. Even though she carries a one-of-a-kind hextech rifle,
-          Caitlyn’s most powerful weapon is her superior intellect, allowing her
-          to lay elaborate traps for any lawbreakers foolish enough to operate
-          in the City of Progress.
-        </p>
+      <div className={styles.blocTabs}>
+        <button
+          className={toggleState === 1 ? styles.activeTabs : styles.tabs}
+          onClick={() => toggleTab(1)}
+        >
+          Tab 1
+        </button>
+        <button
+          className={toggleState === 2 ? styles.activeTabs : styles.tabs}
+          onClick={() => toggleTab(2)}
+        >
+          Tab 2
+        </button>
+        <button
+          className={toggleState === 3 ? styles.activeTabs : styles.tabs}
+          onClick={() => toggleTab(3)}
+        >
+          Tab 3
+        </button>
+      </div>
+      <div className="mx-2">
+        <div
+          className={toggleState === 1 ? styles.activeContent : styles.content}
+        >
+          <h2>Content 1</h2>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+            praesentium incidunt quia aspernatur quasi quidem facilis quo nihil
+            vel voluptatum?
+          </p>
+        </div>
+
+        <div
+          className={toggleState === 2 ? styles.activeContent : styles.content}
+        >
+          <h2>Content 2</h2>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+            voluptatum qui adipisci.
+          </p>
+        </div>
+
+        <div
+          className={toggleState === 3 ? styles.activeContent : styles.content}
+        >
+          <h2>Content 3</h2>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed
+            nostrum rerum laudantium totam unde adipisci incidunt modi alias!
+            Accusamus in quia odit aspernatur provident et ad vel distinctio
+            recusandae totam quidem repudiandae omnis veritatis nostrum
+            laboriosam architecto optio rem, dignissimos voluptatum beatae
+            aperiam voluptatem atque. Beatae rerum dolores sunt.
+          </p>
+        </div>
       </div>
     </div>
   );
