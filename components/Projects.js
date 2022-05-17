6d0adc70c50e06fe21eb91df3c92640d6d1c1ab5
Ali P. Bouraki <ali_panahi10@hotmail.com>
2022-05-17 12:29:46 +0100

    updated projects

--- components/Projects.js
+++ components/Projects.js
@@ -6,12 +6,12 @@ function Projects() {
     <div id="projectsArea">
       <h1 className="text-white text-center p-12 text-2xl">Projects</h1>
       <p className="text-white text-center text-sm mb-12">
-        Some projects I've been working on recently
+        Projects I've been working on recently
       </p>
       {/* Container */}
-      <div className="sm:flex sm:w-[80%] sm:m-auto flex-row sm:space-y-0 space-y-4 sm:space-x-8 justify-center ">
+      <div className="grid sm:grid-cols-3 grid-cols-1  w-full sm:w-[85%] gap-5  sm:m-auto  justify-center ">
         {/* Box 1  */}
-        <div className="border-2 rounded-2xl  sm:w-1/3 backdrop-blur-sm pt-2">
+        <div className="border-2 rounded-2xl   backdrop-blur-sm pt-2">
           <div className="mx-4 text-white">
             <div className="flex justify-between items-center">
               <div>
@@ -47,7 +47,7 @@ function Projects() {
               </button>
             </div>
 
-            <div className="flex justify-end text-xs text-[#fd7f20] space-x-4 p-4 sm:pr-0">
+            <div className="flex justify-end text-xs text-[#fd7f20] space-x-4 p-4 sm:pr-0  md:flex-wrap">
               <div className="border-2 border-[#fd7f20] p-1 rounded-md">
                 Next.js
               </div>
@@ -61,7 +61,7 @@ function Projects() {
           </div>
         </div>
         {/* Box 2  */}
-        <div className="border-2 rounded-2xl  sm:w-1/3 backdrop-blur-sm pt-2">
+        <div className="border-2 rounded-2xl  backdrop-blur-sm pt-2">
           <div className="mx-4 text-white">
             <div className="flex justify-between items-center">
               <div>
@@ -86,17 +86,7 @@ function Projects() {
               An online portfolio demostrating user profile, skill, socials and
               projects
             </p>
-            <div className="flex justify-end ">
-              {/* <button
-                className="mt-[25%] text-right hover:underline cursor-pointer hover:text-[#fd7f20]"
-                onClick={() =>
-                  window.open("https://air-bnb-clone-ebon.vercel.app/")
-                }
-              >
-                View Demo &gt;
-              </button> */}
-            </div>
-            <div className="flex justify-end text-xs text-[#fd7f20] space-x-4 sm:bottom-4 sm:right-4 sm:fixed p-4 sm:p-0 ">
+            <div className="flex justify-end text-xs text-[#fd7f20] space-x-4 sm:bottom-4 sm:right-4 sm:fixed p-4 sm:p-0 md:flex-wrap ">
               <div className="border-2 border-[#fd7f20] p-1 rounded-md">
                 Next.js
               </div>
@@ -113,7 +103,7 @@ function Projects() {
           </div>
         </div>
         {/* Box 3  */}
-        <div className="border-2 rounded-2xl  sm:w-1/3 backdrop-blur-sm pt-2">
+        <div className="border-2 rounded-2xl  backdrop-blur-sm pt-2">
           <div className="mx-4 text-white">
             <div className="flex justify-between items-center">
               <div>
@@ -148,7 +138,7 @@ function Projects() {
                 View Demo &gt;
               </button>
             </div>
-            <div className="flex justify-end text-xs text-[#fd7f20] space-x-4 sm:bottom-4 sm:right-4 sm:fixed p-4 sm:p-0">
+            <div className="flex justify-end text-xs text-[#fd7f20] space-x-4 sm:bottom-4 sm:right-4 sm:fixed p-4 sm:p-0  md:flex-wrap">
               <div className="border-2 border-[#fd7f20] p-1 rounded-md">
                 JavaScript
               </div>
@@ -161,9 +151,60 @@ function Projects() {
             </div>
           </div>
         </div>
+
+        {/* box 4 */}
+        <div className="border-2 rounded-2xl   backdrop-blur-sm pt-2">
+          <div className="mx-4 text-white">
+            <div className="flex justify-between items-center">
+              <div>
+                <h3>Medium</h3>
+              </div>
+              <div
+                className="relative h-8 w-8 md:h-12 cursor-pointer my-auto 
+            transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 "
+              >
+                <Image
+                  src="https://i.imgur.com/tMgGlnE.png"
+                  layout="fill"
+                  objectFit="contain"
+                  objectPosition="right"
+                  onClick={() => window.open("https://github.com/APB10/Medium")}
+                />
+              </div>
+            </div>
+            <p className="mt-8">
+              A medium clone project used to post blog content by other users
+              from sanity CMS. Also, allowing public to post comments within
+              each blog but requires approval first from Sanity to be displayed.
+            </p>
+            <div className="flex justify-end">
+              <button
+                className="mt-[25%] text-right hover:underline cursor-pointer hover:text-[#fd7f20]"
+                onClick={() => window.open("http://medium-apb10.vercel.app/")}
+              >
+                View Demo &gt;
+              </button>
+            </div>
+
+            <div className="flex justify-end text-xs text-[#fd7f20] space-x-4 p-4 sm:pr-0  md:flex-wrap">
+              <div className="border-2 border-[#fd7f20] p-1 rounded-md">
+                Next.js
+              </div>
+              <div className="border-2 border-[#fd7f20] p-1 rounded-md">
+                Sanity
+              </div>
+              <div className="border-2 border-[#fd7f20] p-1 rounded-md">
+                TypeScript
+              </div>
+              <div className="border-2 border-[#fd7f20] p-1 rounded-md">
+                Tailwind
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       {/* change width to 80% upon 3 projects  */}
-      <div className="flex justify-end  w-[80%] m-auto mt-4">
+      <div className="flex  w-[80%] m-auto mt-4">
         <button
           className="text-white  cursor-pointer hover:text-[#fd7f20]"
           onClick={() => window.open("https://github.com/APB10")}
